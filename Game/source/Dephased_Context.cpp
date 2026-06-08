e7a99e46c0f3c7d413322f22a6182504fe96df03
MattScott111 <matthewpomeranz@gmail.com>
2026-06-08 14:18:50 -0400

    Delete Game directory

--- Game/source/Dephased_Context.cpp
+++
@@ -1,8 +0,0 @@
-#include "DePhased_Context.h"
-
-namespace DePhased {
-    // THE MASTER ALLOCATION
-    // This physically reserves the contiguous block of memory for the entire game.
-    // Zero-initialized automatically.
-    GameContext GContext = {0}; 
-}
--- Game/source/EngineCore.cpp
+++
@@ -1,7 +0,0 @@
-#include "../include/EngineCore.h"
-
-namespace DePhased {
-    // Single point of definition for global state
-    EngineContext GContext;
-    bool IsDevModeVisible = false;
-}
